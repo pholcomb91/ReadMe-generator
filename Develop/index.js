5c5997ef4efed4c81249b124426a7a377ce601f1
Peter Holcomb <pholcomb91@gmail.com>
2022-09-29 12:16:18 -0700

    feat: initiates and asks all questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,14 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+const inquirer = require('inquirer');
 
+const generateReadme= ({motivation, why, problem, learn, standOut}) => ``
+;
 // TODO: Create an array of questions for user input
 const questions = [
         "What is your motivation?"
     , 
-        "Why did you build this?"
+        "For what purpose did you build this?"
     ,
         "What problem does it solve?"
     , 
@@ -13,16 +16,45 @@ const questions = [
     , 
         "What makes this project stand out?"
     ];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your project name?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'motivation',
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'why'
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'problem'
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'learn'
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'standOut'
+        }
+    ])
+    .then((responses) => {
+        const readmePageContent = generateReadme(responses);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile('log.txt', process.argv[2], (err) =>
-  err ? console.error(err) : console.log('Success!')
-);
-}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+        // TODO: Create a function to write README file
+        fs.writeFile('README.md', readmePageContent, (err) =>
+        err ? console.error(err) : console.log('Success, you have written a professional Readme.md')
+        )
+    });
+    
